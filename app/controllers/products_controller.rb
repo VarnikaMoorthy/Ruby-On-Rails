be4fc3b1e12232780abc2998fb6260af2964e2e2
Varnika Moorthy <varnikamoorthy7788@example.com>
2025-10-13 23:43:23 +0530

    Products listing working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @message = "Welcome to Ecommerce Store!"
   end
-
+  
   def show
-    @product = Product.find(params[:id])
-  end
+  @product = Product.find(params[:id])
+end
+
 end
