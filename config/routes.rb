373e47abf82b3454f29c6dcbac22f7259164828d
Varnika Moorthy <varnikamoorthy7788@example.com>
2025-10-13 23:54:35 +0530

    CRUD included with css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # Root route
   root "products#index"
-
-  # Health check route
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Resourceful routes for products
   resources :products
 end
